788b50b15272c07ba123791cefddff34eaea8832
Tomas Maly <tmaly1980@gmail.com>
2017-10-06 10:33:23 -0700

    Adding filesystem loader to read image from disk

--- App.js
+++ App.js
@@ -8,15 +8,39 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  Text, TouchableOpacity, Image,
+  View, Dimensions
 } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default class App extends Component {
+  state = {
+    image: null
+  }
+  camera() {
+    ImagePicker.openCamera({
+      width: 1000, // crop width
+      height: 1000, // crop height
+      cropping: true
+    }).then(image => {
+      console.log(image);
+      this.setState({ image })
+    });
+  }
   render() {
     return (
     <View style={styles.container}>
-      <Text>HELLO WORLD</Text>
+
+      <View style={styles.preview}>
+        { this.state.image ? (
+          <View style={styles.imageRow}>
+            <Image style={styles.image} resizeMode='contain' source={require(this.state.image.path)} />
+          </View>
+        ) : (<Text>Please take a photo first</Text>)}
+      </View>
+      <View style={styles.controls}>
+        <TouchableOpacity style={styles.button} onPress={() => this.camera()}><Text style={styles.buttonText}>TAKE PHOTO</Text></TouchableOpacity>
+      </View>
     </View>
     );
   }
@@ -25,20 +49,33 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    marginVertical: 25,
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button: {
+    backgroundColor: '#33CC33',
+    padding: 10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonText: {
+    color: '#000'
   },
+  preview: {
+    flex: 1,
+    backgroundColor: '#0f0',
+    width: Dimensions.get('window').width - 25,
+    margin: 10
+  },
+  imageRow: {
+    width: 200,
+    height: 200
+  },
+  image: {
+    flex: 1,
+    width: undefined,
+    height: undefined
+  }
 });
 
 AppRegistry.registerComponent('CameraUploadCropDemo41', () => CameraUploadCropDemo41);
---
+++ ios/Pods/Target Support Files/Pods-CameraUploadCropDemo41/Pods-CameraUploadCropDemo41-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CameraUploadCropDemo41 : NSObject
+@end
+@implementation PodsDummy_Pods_CameraUploadCropDemo41
+@end
